51d3f1ec2f55d34cd5a84847ee86a2d4b031c048
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-05-20 11:27:14 -0400

    adding components

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
-const multer = require("multer")
+// const multer = require("multer")
 
 //get routes 
 const posts = require("./routes/api/posts.js")
