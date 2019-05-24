26decc094b98a4d481da6fa1226a379820789ad0
Hayden Bryant <haydenbryant.student@careerdevs.com>
2019-05-24 17:44:51 -0400

    fixed router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch}  from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
+import Post from "./components/Post";
+
 
 class App extends Component {
   render() {
